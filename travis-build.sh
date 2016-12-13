ec2d8f4a9ac9faf8e5e9cffbe9810e86e0368992
Alexey Zvegintcev <alexey.zvegintcev@sysgears.com>
2016-12-13 16:38:02 +0200

    Added publishing to bintray

--- travis-build.sh
+++ travis-build.sh
@@ -3,3 +3,14 @@
 echo "### Running tests"
 
 cd dao-app && ./gradlew test
+
+if [[ $TRAVIS_BRANCH == 'grails3' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+	echo "### publishing plugin to bintray"
+	gradle bintrayUpload
+
+else
+  echo "Not on master branch, so not publishing"
+  echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+fi
